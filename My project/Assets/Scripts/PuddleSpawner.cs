726a5326d1488638b2bc1fd4770ec22ad22768bb
Lilli <lillichuawiwat@gmail.com>
2024-05-20 15:30:56 +1000

    Save PuddleSpawner

--- My project/Assets/Scripts/PuddleSpawner.cs
+++ My project/Assets/Scripts/PuddleSpawner.cs
@@ -18,13 +18,13 @@ public class PuddleSpawner : GameBehaviour
     // Update is called once per frame
     public void SpawnPuddle()
     {
-        if(_GM.state == GameManager.GameState.Playing)
+        if(_GM.gameState == GameManager.GameState.Playing)
         {
             Vector3 position = new Vector3(Random.Range(minX,maxX), 0.49f,Random.Range(minZ,maxZ));
 
             _GM.shootManager.SpawnReloadPuddle(position);
 
-            ExecutueAfterSeconds(Random.Range(minSeconds,maxSeconds),()=> SpawnPuddle());
+            ExecuteAfterSeconds(Random.Range(minSeconds,maxSeconds),()=> SpawnPuddle());
         }
         
     }
